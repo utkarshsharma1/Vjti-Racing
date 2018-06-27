e3cba0aecb161ba4401929570ab9630481dfe24a
Utkarsh Sharma <utkarshsharma650@gmail.com>
2018-06-27 11:48:34 +0530

    fevv

--- finaltsthall13may/finaltsthall13may.ino
+++ finaltsthall13may/finaltsthall13may.ino
@@ -1,48 +1,47 @@
+#include <EEPROM.h>
+#include <SPI.h>
+#include <SD.h>
+
 volatile double rpm_1=0; 
-volatile double rpm_2=0; 
 volatile unsigned long timeold1=0;
 volatile unsigned long timenew_1=0;
-volatile unsigned long timeold2=0;
-volatile unsigned long timenew_2=0;
 volatile double diff_1=0;
-volatile double diff_2=0;
 volatile int i1 = 0;
-volatile int i2 = 0;
-volatile float cvt=0;
+//volatile float cvt=0;
+float speed;
+int address = 0;
+int old_rpm;
+const int chipSelect = 4;
+void clearEEPROM();
+void writeEeprom(int speed);
 
 void setup()
 { 
-  //lcd.begin(16,2);
   pinMode(3,INPUT_PULLUP);   //Primary 
-  pinMode(2,INPUT_PULLUP);   //Secondary
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, LOW);
   attachInterrupt(digitalPinToInterrupt(3), rpm_fun_1,FALLING);  
-  attachInterrupt(digitalPinToInterrupt(2), rpm_fun_2,FALLING);  
   rpm_1 = 0;//Driving Pulley 
-  rpm_2 = 0;//Driven Pulley 
   timeold1 = 0;
   timenew_1=0;
-  timeold2 = 0;
-  timenew_2=0;
+  old_rpm = 0;
+  speed = 0;
   Serial.begin(9600);
+  clearEEPROM();
 
-  //SDCARD_SETUP
-
-//Open serial communications and wait for port to open:
-  //Serial.begin(250000);
-  /*pinMode(53, OUTPUT);
-  while (!Serial) 
-  {
+  /*while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-Serial.print("Initializing SD card...");
-if (!SD.begin(chipSelect)) 
-  {
-    Serial.println("initialization failed!");
-    return;
+  Serial.print("Initializing SD card...");
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    while (1);
   }
-Serial.println("initialization done.");
-
-}*/
+  Serial.println("card initialized."); */ 
 }
 
 void rpm_fun_1()
@@ -54,14 +53,12 @@ void rpm_fun_1()
   }
   else
   {
-  diff_1=timenew_1-timeold1;
-  //Serial.println(diff_1);
+    diff_1=timenew_1-timeold1;
   }
   if(i1==0)
   { 
     rpm_1=(60000000.0/diff_1);
-    timeold1=timenew_1;
-    //Serial.println(diff_1);  
+    timeold1=timenew_1; 
   }
   i1++;
   if(i1==1)
@@ -71,98 +68,68 @@ void rpm_fun_1()
   diff_1 = 0;
 }
 
-void rpm_fun_2()
-{
-  timenew_2=micros();
-  if(timenew_2==timeold2)
- { rpm_2=0;}
-  else
-  {
-  diff_2=timenew_2-timeold2;
-  //Serial.println(diff_2);
-  }
-  if(i2==0)
-  { 
-    rpm_2=(60000000.0/diff_2);
-    timeold2=timenew_2;
-    //Serial.println(diff_2);
-    
-  }
-  i2++;
-  if(i2==3)
-  {
-    i2=0;
-  }
-  diff_2 = 0;
-}
-
 void loop()
 { 
- // cvt=rpm_1/(rpm_2*11.9025);
- // if(cvt>0.5&&cvt<3.2)
- // {
-    Serial.print(rpm_1);
-    Serial.print("       ");
- /*   Serial.print(rpm_2);
-    Serial.print("       ");
-    Serial.print(cvt);
-    Serial.print("       ");
-    //Serial.println(millis());
-   */ delay(100);
-    //lcd.clear();
-    //lcd.setCursor(0,0);
-    //lcd.print(cvt);
-    //lcd.setCursor(0,1);
-    Serial.println(2*18*3.142*rpm_1*0.254/(5*60));
- // }
-  /*else
+  //cvt=rpm_1/(rpm_2*11.9025);
+// if(cvt>0.5&&cvt<3.2)
+  Serial.println(digitalRead(3));
+  if (rpm_1 < 600)
   {
-    lcd.setCursor(0,0);
-    lcd.print("VJTI");
-    lcd.setCursor(0,1);
-    lcd.print("Racing");
-    }*/
+    if (old_rpm != rpm_1)
+    {
+      Serial.print(rpm_1);
+      Serial.print("       ");
+      speed = (2*18*3.142*rpm_1*0.2794/(5*60));
+      Serial.println(speed);
+     // Serial.print("       ");
+      //Serial.print(cvt);
+      //Serial.print("       ");
+     // Serial.println(millis());
+      delay(100);
+/*      writeEeprom(speed);
+      File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+      // if the file is available, write to it:
+      if (dataFile) {
+        dataFile.println(rpm_1);
+        dataFile.close();
+        // print to the serial port too:
+      }
+      // if the file isn't open, pop up an error:
+      else {
+        Serial.println("error opening datalog.txt");
+      }*/
+    }
+    old_rpm = rpm_1;
+  } 
 }
     
 
-/*File Driving = SD.open("rpm1.txt", FILE_WRITE);
-// if the file is available, write to it:
-if (Driving) 
-{
-Driving.println(rpm_1);
-Driving.close();
-} 
-// if the file isn't open, pop up an error:
-else {
-Serial.println("error opening test.txt");
+void writeEeprom(int speed){
+  int value = speed;
+//  byte sen = sensor;
+//  EEPROM.write(address, sen);
+//  address++;
+  EEPROM.write(address, value);
+  address++;
+  Serial.print("Data written in address ");
+  Serial.println(address);
+  if(address == EEPROM.length())
+  {
+    address = 0;
+  }
 }
 
-File Driven = SD.open("rpm2.txt", FILE_WRITE);
-// if the file is available, write to it:
-if (Driven) 
+void clearEEPROM()
 {
-Driven.println(rpm_2);
-Driven.close();
-} 
-// if the file isn't open, pop up an error:
-else {
-Serial.println("error opening test.txt");
-
+  for(int i = 0; i<EEPROM.length(); i++){
+    //if(EEPROM.read(i)!=0){
+      EEPROM.write(i,0);
+  //  }
+  }
+  Serial.println("EEPROM erased");
+  address = 0;
 }
-
-File timing = SD.open("time.txt", FILE_WRITE);
-// if the file is available, write to it:
-if (timing) 
-
-{
-timing.println(millis());
-timing.close();
-} 
-// if the file isn't open, pop up an error:
-else {
-Serial.println("error opening time.txt");
-*/
-
 
 
 
---
+++ ultrasonic_ir_temp/ultrasonic_ir_temp.ino
@@ -0,0 +1,48 @@
+#include <Wire.h>
+#include <Adafruit_MLX90614.h>
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+const int trigPin = 9;
+const int echoPin = 10;
+float sum;
+float avg_dist[5] = {0,0,0,0,0};
+
+float duration, distance;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+  Serial.println("Adafruit MLX90614 test");  
+  mlx.begin();  
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration*.034)/2;
+  Serial.print("Distance: ");
+  Serial.println(Average_dist(distance));
+
+  if(mlx.readObjectTempC()<500){
+    Serial.println(mlx.readObjectTempC());
+  }
+   
+  delay(1000);
+}
+
+float Average_dist(float dist){
+  avg_dist[4] = dist;
+  sum = 0;
+  for(int i =0; i < 5; i++){
+    sum = sum + avg_dist[i];
+  }
+  for(int i =0;i<4;i++){
+    avg_dist[i] = avg_dist[i+1];
+  }
+  return dist = sum/5;
+}
